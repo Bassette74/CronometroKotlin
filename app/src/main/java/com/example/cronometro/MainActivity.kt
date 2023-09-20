eaed53786145c5791df016b9d81b5021eca6f2b4
Bassette74 <felipe.bassette@hotmail.com>
2023-09-19 23:42:09 -0300

    primeiro commit
    Criação da primeira activity mas apenas o layout

---
+++ app/src/main/java/com/example/cronometro/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.cronometro
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.SystemClock
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    }
